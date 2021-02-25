7a2d313b9037746ed77333778e4012034d7bf2e2
Thales <thales.c.santana@outlook.com>
2021-02-25 00:17:38 -0300

    Challenge Box completed

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -1,10 +1,13 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
 import styles from '../styles/components/CompletedChallenges.module.css'
 
 export function CompletedChalenges(){
+    const {challengesCompleted} = useContext(ChallengesContext)
     return(
         <div className={styles.completedChalengesContainer}>
             <span>Desafios Completos</span>
-            <span>00</span>
+            <span>{challengesCompleted}</span>
         </div>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,9 @@ import { CompletedChalenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
+import { ChallengeBox } from '../components/ChallengeBox';
 
 import Head from 'next/head'
-
 import styles from '../styles/pages/Home.module.css'
 
 export default function Home() {
@@ -23,7 +23,7 @@ export default function Home() {
           <Countdown />
         </div>
         <div>
-          
+          <ChallengeBox />
         </div>
       </section>
     </div>
